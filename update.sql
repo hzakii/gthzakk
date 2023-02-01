8ca9be9fffbc2cc49ac6fcd2d1e2ba4e8f3269fd
hzakii <hzakii@live.com>
2023-02-01 18:29:43 +0100

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,4 +1,4 @@
-INSERT INTO airport (icao, iata, name, long, lat, category) VALUES ("DAOO", "ORN", "oran", "N36", "E009", "2");
+INSERT INTO airport (icao, iata, name, long, lat, category) VALUES ("DABT", "ORN", "oran", "N36", "E009", "2");
 INSERT INTO airport (icao, iata, name, long, lat, category) VALUES ("DABC", "CZL", "constantine", "N36", "E009", "2");
 INSERT INTO airport (icao, iata, name, long, lat, category) VALUES ("DAAT", "TMR", "tamanrasset", "N36", "E009", "2");
 INSERT INTO airport (icao, iata, name, long, lat, category) VALUES ("DAAJ", "DJG", "djanet", "N36", "E009", "2");
